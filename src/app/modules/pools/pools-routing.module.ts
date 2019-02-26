3fa0b0d4d7491ff392bd47bed38dac8021e24883
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-02-26 09:13:10 -0500

    Added pools endpoint

--- src/app/modules/pools/pools-routing.module.ts
+++ src/app/modules/pools/pools-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PoolComponent } from './components/pool/pool.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PoolComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/pools/pools.module.ts
+++ src/app/modules/pools/pools.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PoolsRoutingModule } from './pools-routing.module';
+import { PoolComponent } from './components/pool/pool.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PoolComponent],
   imports: [
     CommonModule,
     PoolsRoutingModule
