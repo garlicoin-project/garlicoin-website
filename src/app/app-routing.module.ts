a7174c01afe77377f2155e0a36367ebee5a235a1
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-02-26 08:57:43 -0500

    Fixed wierd bug with the navigation (might be able to be reverted later)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     children: [
       {
         // Defines a lazy loaded route at "/" with the home module
-        path: '',
+        path: 'home',
         loadChildren: './modules/home/home.module#HomeModule'
       },
       {
@@ -31,11 +31,11 @@ const routes: Routes = [
         loadChildren: './modules/pools/pools.module#PoolsModule'
       },
       // New routes go before the catch all
-      // {
-      //   // Redirects all unmatched routes to home
-      //   path: '**',
-      //   redirectTo: ''
-      // }
+      {
+        // Redirects all unmatched routes to home
+        path: '**',
+        redirectTo: 'home'
+      }
     ]
   }
 ];
