c3bd593c32311eec5f5bf9a735ca0a2081f015aa
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-02-25 23:09:57 -0500

    Generated empty modules

---
+++ src/app/modules/downloads/downloads.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DownloadsRoutingModule } from './downloads-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DownloadsRoutingModule
+  ]
+})
+export class DownloadsModule { }
---
+++ src/app/modules/guide/guide.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GuideRoutingModule } from './guide-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GuideRoutingModule
+  ]
+})
+export class GuideModule { }
